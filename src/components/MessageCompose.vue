4a86fbe7880ea41c5b16938a205026cf43b9ea3a
Prajwal Shrestha <pjmessi25@gmail.com>
2020-12-29 22:39:06 +0545

    initial commit

---
+++ src/components/MessageCompose.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Type your message" />
+      <button class="btn btn-primary">Send</button>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="content">
+      <div class="container p-0">
+        <h1 class="h3 mb-3">Messages</h1>
+
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-12 col-lg-5 col-xl-3 border-right">
+              <Contact/>
+            </div>
+
+            <div class="col-12 col-lg-7 col-xl-9">
+              
+              <MessageList/>
+
+              <div class="flex-grow-0 py-3 px-4 border-top">
+                <MessageCompose/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Contact from '@/components/Contact';
+import MessageList from '@/components/MessageList';
+import MessageCompose from '@/components/MessageCompose';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Contact, MessageList, MessageCompose }
+};
+
 </script>
+
+
+<style scoped>
+  body {
+    margin-top: 20px;
+  }
+
+  .chat-online {
+    color: #34ce57;
+  }
+
+  .chat-offline {
+    color: #e4606d;
+  }
+
+  .chat-messages {
+    display: flex;
+    flex-direction: column;
+    max-height: 800px;
+    overflow-y: scroll;
+  }
+
+  .chat-message-left,
+  .chat-message-right {
+    display: flex;
+    flex-shrink: 0;
+  }
+
+  .chat-message-left {
+    margin-right: auto;
+  }
+
+  .chat-message-right {
+    flex-direction: row-reverse;
+    margin-left: auto;
+  }
+  .py-3 {
+    padding-top: 1rem !important;
+    padding-bottom: 1rem !important;
+  }
+  .px-4 {
+    padding-right: 1.5rem !important;
+    padding-left: 1.5rem !important;
+  }
+  .flex-grow-0 {
+    flex-grow: 0 !important;
+  }
+  .border-top {
+    border-top: 1px solid #dee2e6 !important;
+  }
+</style>
+
